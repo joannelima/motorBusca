db577a01f697332f80bc5d67b77f264c62350e73
Joanne Lima <joanne.lima@ifood.com.br>
2024-07-05 03:41:46 -0300

    organizando motor

--- src/SearchEngine.java
+++ src/SearchEngine.java
@@ -13,6 +13,87 @@ import java.util.Collections;
 import java.util.LinkedHashMap;
 
 public class SearchEngine {
+
+    public static void main(String[] args) {
+        SearchEngine engine = new SearchEngine("src/verbetesWikipedia.xml");
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Digite as palavras que deseja buscar (separadas por espaço), ou 'sair' para encerrar:");
+            String input = scanner.nextLine().toLowerCase();
+
+            if (input.equals("sair")) {
+                break;
+            }
+
+            String[] queries = input.split("\\s+");
+            List<Map.Entry<Integer, Integer>> results = engine.search(queries);
+
+            // Mostra os 5 primeiros resultados da pesquisa
+            if (!results.isEmpty()) {
+                System.out.println("Top 5 páginas para '" + String.join(" ", queries) + "':");
+                for (int i = 0; i < Math.min(5, results.size()); i++) {
+                    Map.Entry<Integer, Integer> entry = results.get(i);
+                    System.out.println("Página " + entry.getKey() + ": " + entry.getValue() + " ocorrências");
+                }
+            } else {
+                System.out.println("Nenhum resultado foi encontrado para a sua pesquisa");
+            }
+        }
+
+        scanner.close();
+    }
+
+    public List<Map.Entry<Integer, Integer>> search(String[] queries) {
+        String key = String.join(" ", queries) + " AND"; // Cria uma chave para o cache
+
+        if (cache.containsKey(key)) {
+            System.out.println("Resultado encontrado no cache.");
+            return cache.get(key);
+        }
+
+        Map<Integer, Integer> combinedResults;
+
+        if (queries.length == 0) return Collections.emptyList();
+
+        // Busca a primeira palavra
+        String firstQuery = queries[0].toLowerCase();
+        Map<Integer, Integer> pageCountMap = new HashMap<>(wordPageCountMap.getOrDefault(firstQuery, Collections.emptyMap()));
+
+        // Interseção das páginas com todas as palavras
+        for (int i = 1; i < queries.length; i++) {
+            String query = queries[i].toLowerCase();
+            Map<Integer, Integer> currentMap = wordPageCountMap.getOrDefault(query, Collections.emptyMap());
+
+            // Atualiza o map com a interseção das páginas
+            pageCountMap.keySet().retainAll(currentMap.keySet());
+            for (Map.Entry<Integer, Integer> entry : pageCountMap.entrySet()) {
+                if (currentMap.containsKey(entry.getKey())) {
+                    entry.setValue(Math.min(entry.getValue(), currentMap.get(entry.getKey())));
+                } else {
+                    pageCountMap.remove(entry.getKey());
+                }
+            }
+        }
+
+        if (pageCountMap.isEmpty()) {
+            List<Map.Entry<Integer, Integer>> orResults = searchWithOr(queries);
+            cache.put(String.join(" ", queries) + " OR", orResults);
+
+            return orResults;
+        }
+
+        combinedResults = pageCountMap;
+
+        List<Map.Entry<Integer, Integer>> pageList = new ArrayList<>(combinedResults.entrySet());
+        pageList.sort((e1, e2) -> e2.getValue().compareTo(e1.getValue())); // Ordena por contagem de ocorrências
+
+        cache.put(key, pageList);
+
+        return pageList;
+    }
+
     private Map<String, Map<Integer, Integer>> wordPageCountMap; // Hash invertido: palavra -> (página -> contagem)
     private LinkedHashMap<String, List<Map.Entry<Integer, Integer>>> cache; //lista encadeada
     private final int CACHE_SIZE = 5; // cache
@@ -92,54 +173,6 @@ public class SearchEngine {
         pageCountMap.put(pageIndex, pageCountMap.getOrDefault(pageIndex, 0) + count);
     }
 
-    public List<Map.Entry<Integer, Integer>> search(String[] queries) {
-        String key = String.join(" ", queries) + " AND"; // Cria uma chave para o cache
-
-        if (cache.containsKey(key)) {
-            System.out.println("Resultado encontrado no cache.");
-            return cache.get(key);
-        }
-
-        Map<Integer, Integer> combinedResults = new HashMap<>();
-
-        if (queries.length == 0) return Collections.emptyList();
-
-        // Busca a primeira palavra
-        String firstQuery = queries[0].toLowerCase();
-        Map<Integer, Integer> pageCountMap = new HashMap<>(wordPageCountMap.getOrDefault(firstQuery, Collections.emptyMap()));
-
-        // Interseção das páginas com todas as palavras
-        for (int i = 1; i < queries.length; i++) {
-            String query = queries[i].toLowerCase();
-            Map<Integer, Integer> currentMap = wordPageCountMap.getOrDefault(query, Collections.emptyMap());
-
-            // Atualiza o map com a interseção das páginas
-            pageCountMap.keySet().retainAll(currentMap.keySet());
-            for (Map.Entry<Integer, Integer> entry : pageCountMap.entrySet()) {
-                if (currentMap.containsKey(entry.getKey())) {
-                    entry.setValue(Math.min(entry.getValue(), currentMap.get(entry.getKey())));
-                } else {
-                    pageCountMap.remove(entry.getKey());
-                }
-            }
-        }
-
-        if (pageCountMap.isEmpty()) {
-            List<Map.Entry<Integer, Integer>> orResults = searchWithOr(queries);
-            cache.put(String.join(" ", queries) + " OR", orResults);
-
-            return orResults;
-        }
-
-        combinedResults = pageCountMap;
-
-        List<Map.Entry<Integer, Integer>> pageList = new ArrayList<>(combinedResults.entrySet());
-        pageList.sort((e1, e2) -> e2.getValue().compareTo(e1.getValue())); // Ordena por contagem de ocorrências
-
-        cache.put(key, pageList);
-
-        return pageList;
-    }
 
     private List<Map.Entry<Integer, Integer>> searchWithOr(String[] queries) {
         Map<Integer, Integer> combinedResults = new HashMap<>();
@@ -159,36 +192,5 @@ public class SearchEngine {
         pageList.sort((e1, e2) -> e2.getValue().compareTo(e1.getValue())); // Ordena por contagem de ocorrências
 
         return pageList;
-    }
-
-    public static void main(String[] args) {
-        SearchEngine engine = new SearchEngine("src/verbetesWikipedia.xml");
-
-        Scanner scanner = new Scanner(System.in);
-
-        while (true) {
-            System.out.println("Digite as palavras que deseja buscar (separadas por espaço), ou 'sair' para encerrar:");
-            String input = scanner.nextLine().toLowerCase();
-
-            if (input.equals("sair")) {
-                break;
-            }
-
-            String[] queries = input.split("\\s+");
-            List<Map.Entry<Integer, Integer>> results = engine.search(queries);
-
-            // Mostra os 5 primeiros resultados da pesquisa
-            if (!results.isEmpty()) {
-                System.out.println("Top 5 páginas para '" + String.join(" ", queries) + "':");
-                for (int i = 0; i < Math.min(5, results.size()); i++) {
-                    Map.Entry<Integer, Integer> entry = results.get(i);
-                    System.out.println("Página " + entry.getKey() + ": " + entry.getValue() + " ocorrências");
-                }
-            } else {
-                System.out.println("Nenhum resultado foi encontrado para a sua pesquisa");
-            }
-        }
-
-        scanner.close();
     }
 }
